55d36981c3bdb5cffc269020edb845b8038dcd76
Costa <keith@Keiths-MacBook-Pro.local>
2019-04-30 18:28:19 -0400

    plandemarketing

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -42,14 +42,39 @@ $("#submit").on("click", function () {
 
 })
 
+$("#submitEs").on("click", function () {
+    //Pull info and store in variables
+    name =  $("#name").val();
+    email =  $("#email").val();
+    message = $("#message").val();
+
+    console.log (name, email, message)
+
+    //Push up to database
+    database.ref().push({
+        name: name,
+        email: email,
+        message: message,
+    })
+
+    //Empty Conact & submit button
+    $('#contactform').empty();
+    $("#submitrow").empty();
+
+    //Fill Contact with Thanks Message
+    $("#contactform").html("<div class='col-12 border-0 rounded font green pl-4 py-3 rounded bg-white'> Estimado " + name + ", <br><br>Gracias por ponerte en contacto conmigo. Aprecio su interes en mi trabajo y servicios. Le mandare un mensage a " + email+ " lo antes possible.<br><br> Sinceramente, Keith Costa</div>")
+
+    $("#contactform").addClass("p-3")
+
+})
 //NAVBAR SCROLL
 
 $(window).scroll(function(){
     $("#navbar").toggleClass("scroll" , $(this).scrollTop()> 10);
     $(".dropdown").toggleClass("scroll" , $(this).scrollTop()>10);
     // $("#navbar").removeClass("scroll" , $(this).scrollTop() = 0.5);
-    $(".logo").toggleClass("scrollImg" , $(this).scrollTop()> 10);
-    
+    $(".logo").toggleClass("scrollImg" , $(this).scrollTop()> 10); 
+    $("#testdiv").toggleClass("test1", $(this).scrollTop()> 10);
 })
 
 //WHITE DROP DOWN MENU
@@ -58,3 +83,9 @@ $("#white").on("click", (event) => {
     $("#navbar").toggleClass("scroll") 
     $(".dropdown").toggleClass("scroll")
 })
+
+//SPANISH TO ENGLISH TRANSLATE
+
+// $(window).scroll(function(){
+//     $("#english").attr("href","index.html#whatido").$(this).scrollTop(800)
+// })
